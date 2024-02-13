2abdb0466a1171be90839cd2b57f17e7259f45aa
crystalhe97 <yuweihe@uw.edu>
2024-02-13 15:42:19 -0800

    v1

--- scrape.py
+++ scrape.py
@@ -1,46 +1,75 @@
-import time
+import re
+import json
+import datetime
+from zoneinfo import ZoneInfo
+import html
+
 import requests
-from bs4 import BeautifulSoup
-import csv
 
-# Function to scrape a single page
-def scrape_page(url):
-    try:
-        res = requests.get(url)
-        # Check if the request was successful
-        if res.status_code == 200:
-            soup = BeautifulSoup(res.text, "html.parser")
-            selector = "div.search-result-preview > div > h3 > a"
-            a_eles = soup.select(selector)
-            return [x['href'] for x in a_eles]
-        else:
-            print(f"Failed to retrieve page: {url}")
-            return []
-    except Exception as e:
-        print(f"Error occurred: {e}")
-        return []
+from db import get_db_conn
 
-# Main scraping function
-def scrape_events(base_url, pages):
-    all_urls = []
-    for i in range(1, pages + 1):
-        time.sleep(1)  # Delay to respect the server
-        page_url = f"{base_url}/page/{i}"
-        urls = scrape_page(page_url)
-        all_urls.extend(urls)
-        print(f"Scraped page {i}, found {len(urls)} URLs")
-    return all_urls
 
-# Base URL for the events list
-base_url = "https://visitseattle.org/events"
-pages = 48
+URL = 'https://visitseattle.org/events/page/'
+URL_LIST_FILE = './data/links.json'
+URL_DETAIL_FILE = './data/data.json'
 
-# Scrape the events
-event_urls = scrape_events(base_url, pages)
-print(event_urls)
+def list_links():
+    res = requests.get(URL + '1/')
+    last_page_no = int(re.findall(r'bpn-last-page-link"><a href=".+?/page/(\d+?)/.+" title="Navigate to last page">', res.text)[0])
 
-# Print the URLs in the requested format
-print("[")
-for url in event_urls:
-    print(f" '{url}',")
-print("]")
+    links = []
+    for page_no in range(1, last_page_no + 1):
+        res = requests.get(URL + str(page_no) + '/')
+        links.extend(re.findall(r'<h3 class="event-title"><a href="(https://visitseattle.org/events/.+?/)" title=".+?">.+?</a></h3>', res.text))
+
+    json.dump(links, open(URL_LIST_FILE, 'w'))
+
+def get_detail_page():
+    links = json.load(open(URL_LIST_FILE, 'r'))
+    data = []
+    for link in links:
+        try:
+            row = {}
+            res = requests.get(link)
+            row['title'] = html.unescape(re.findall(r'<h1 class="page-title" itemprop="headline">(.+?)</h1>', res.text)[0])
+            datetime_venue = re.findall(r'<h4><span>.*?(\d{1,2}/\d{1,2}/\d{4})</span> \| <span>(.+?)</span></h4>', res.text)[0]
+            row['date'] = datetime.datetime.strptime(datetime_venue[0], '%m/%d/%Y').replace(tzinfo=ZoneInfo('America/Los_Angeles')).isoformat()
+            row['venue'] = datetime_venue[1].strip() # remove leading/trailing whitespaces
+            metas = re.findall(r'<a href=".+?" class="button big medium black category">(.+?)</a>', res.text)
+            row['category'] = html.unescape(metas[0])
+            row['location'] = metas[1]
+            data.append(row)
+        except IndexError as e:
+            print(f'Error: {e}')
+            print(f'Link: {link}')
+    json.dump(data, open(URL_DETAIL_FILE, 'w'))
+
+def insert_to_pg():
+    q = '''
+    CREATE TABLE IF NOT EXISTS events (
+        url TEXT PRIMARY KEY,
+        title TEXT,
+        date TIMESTAMP WITH TIME ZONE,
+        venue TEXT,
+        category TEXT,
+        location TEXT
+    );
+    '''
+    conn = get_db_conn()
+    cur = conn.cursor()
+    cur.execute(q)
+    
+    urls = json.load(open(URL_LIST_FILE, 'r'))
+    data = json.load(open(URL_DETAIL_FILE, 'r'))
+    for url, row in zip(urls, data):
+        q = '''
+        INSERT INTO events (url, title, date, venue, category, location)
+        VALUES (%s, %s, %s, %s, %s, %s)
+        ON CONFLICT (url) DO NOTHING;
+        '''
+        cur.execute(q, (url, row['title'], row['date'], row['venue'], row['category'], row['location']))
+
+if __name__ == '__main__':
+    list_links()
+    get_detail_page()
+    insert_to_pg()
