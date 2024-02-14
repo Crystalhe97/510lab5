da86abb9d38da93c6ae73923652e310822b8d56f
crystalhe97 <yuweihe@uw.edu>
2024-02-13 17:37:06 -0800

    update db

--- db.py
+++ db.py
@@ -13,6 +13,7 @@ db_port = os.getenv('DB_PORT')
 db_name = os.getenv('DB_NAME')
 conn_str = f'postgresql://{db_user}:{db_pw}@{db_host}:{db_port}/{db_name}'
 
+print(conn_str)
 def get_db_conn():
     conn = psycopg2.connect(conn_str)
     conn.autocommit = True
